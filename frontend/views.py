b845c6325795345ef228a1207ecbc63c479ec362
Novic09 <tcarlosvargas@gmail.com>
2012-10-05 15:21:20 +0000

    Confuguracion de URLs en el servidor

--- frontend/views.py
+++ frontend/views.py
@@ -1,10 +1,10 @@
 #encode:utf-8
-from frontend.models import Receta, Comentario
+from proyecto.frontend.models import Receta, Comentario
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-	
+
 
 def sobre(request):
 	html = "<html><body>Proyecto de Ejemple en Django<body></html>"
@@ -13,7 +13,7 @@ def sobre(request):
 def inicio(request):
 	recetas = Receta.objects.all()
 	return render_to_response('inicio.html',{'recetas':recetas})
-	
+
 def usuarios(request):
 	usuarios = User.objects.all()
 	recetas = Receta.objects.all()
--- urls.py
+++ urls.py
@@ -5,12 +5,12 @@ from django.conf import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    
-    url(r'^$','frontend.views.inicio'),
-    url(r'^usuarios/$','frontend.views.usuarios'),
-    url(r'^recetas/$','frontend.views.lista_recetas'),
-    url(r'^receta/(?P<id_receta>\d+)$', 'frontend.views.detalle_receta'),
-    url(r'^sobre/$','frontend.views.sobre'),    
+
+    url(r'^$','proyecto.frontend.views.inicio'),
+    url(r'^usuarios/$','proyecto.frontend.views.usuarios'),
+    url(r'^recetas/$','proyecto.frontend.views.lista_recetas'),
+    url(r'^receta/(?P<id_receta>\d+)$', 'proyecto.frontend.views.detalle_receta'),
+    url(r'^sobre/$','proyecto.frontend.views.sobre'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^media/(?P<path>.*)$','django.views.static.serve',
